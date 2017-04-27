3fa35476ce633962b240e582791fac1e15f532f7
typicamike002 <townsendm105@gmail.com>
2017-04-26 21:20:54 -0400

    created route example
    using the api key obtained through steam, I have created a simple route that will take any steam user's id as an argument and the api key as a query.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const request = require('request');
 const router = express.Router();
 
+/**
+ * root
+ */
 router.get('/', (req, res, next) => {
+    res.setHeader('Content-type', 'application/json');
     res.json({
         message: 'Welcome to the steam api'}
+    );
+});
+
+/**
+ * /api/profile/:steamID?key=YOURKEYGOESHERE
+ */
+router.get('/api/profile/:steamID', (req, res, next) => {
+
+    request.get(''
+        + 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/'
+        + '?key=' + req.query.key
+        + '&steamids=' + req.params.steamID
+        , (steamErr, steamRes, steamBody) => {
+            if (steamErr){ 
+                throw new Error(steamErr.message);
+            }
+            
+            res.setHeader('Content-type', 'application/json');
+            res.json(JSON.parse(steamBody));
+        }
     );
 });
 
