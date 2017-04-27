24e0d84c5d31257304f2c662bf20f785867fa8f5
typicamike002 <townsendm105@gmail.com>
2017-04-27 18:48:01 -0400

    added debug

--- routes/index.js
+++ routes/index.js
@@ -18,14 +18,14 @@ router.get('/', (req, res, next) => {
 router.get('/api/profile/:steamID', (req, res, next) => {
 
     request.get(''
-        + 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/'
+        + 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/'
         + '?key=' + req.query.key
         + '&steamids=' + req.params.steamID
         , (steamErr, steamRes, steamBody) => {
             if (steamErr){ 
                 throw new Error(steamErr.message);
             }
-            
+
             res.setHeader('Content-type', 'application/json');
             res.json(JSON.parse(steamBody));
         }
